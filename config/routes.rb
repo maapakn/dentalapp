97849d48cde8b35bf89acea8df72b83b36493ba3
Diego Esparza <diego.esparza@ceinf.cl>
2016-02-15 19:30:23 -0300

    actualizaciones de configuracion

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-  get 'user/create'
-
-  get 'user/destroy'
-
-  get 'user/edit'
-
-  get 'user/index'
-
-  get 'user/show'
-
-  get 'user/update'
 
   resources :pedidos
   resources :materials
   resources :trabajos
   resources :dientes
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers:{registrations: "registrations"}
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
